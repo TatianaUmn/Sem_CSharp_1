8a0d415527028fe6fe6cc03ca4a11b32e19111ac
Tatiana <tatum@rambler.ru>
2022-08-04 23:47:24 +0300

    Task 1

---
+++ Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+Console.WriteLine($"Максимальное число {a}");
+}
+if (a < b)
+{
+Console.WriteLine($"Максимальное число {b}"); 
+}
+else
+{
+    Console.WriteLine("Введено два одинаковых числа");
+}

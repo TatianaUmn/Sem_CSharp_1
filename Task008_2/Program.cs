46180b1747fcaf002e7c95d56c9b6d28371680c2
Tatiana <tatum@rambler.ru>
2022-08-06 23:05:24 +0300

    Task 008 version 2

---
+++ Task008_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите положительное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while(count < N - 1)
+{
+    count = count + 2;
+    Console.Write(count + " ");
+}
